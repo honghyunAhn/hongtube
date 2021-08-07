1cd5322af47d3403cdc63f4cfcf4f33c6c155259
honghyunAhn <anh2073@naver.com>
2021-08-07 17:46:30 +0900

    Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -1,23 +1,20 @@
 import express from "express";
 
 const PORT = 9090;
-
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
     next();
-}
+};
+
 const handleHome = (req, res) => {
-    return res.send("I love middlewares");
-}
-const handleLogin = (req, res) => {
-    return res.send({message:"Login here."});
-}
+    return res.send("I love middlewares.");
+};
 
-app.get("/", gossipMiddleware, handleHome);
-app.get("/login", handleLogin);
+app.get("/", logger, handleHome);
 
-const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`);
+const handleListening = () => 
+    console.log(`✅Server listenting on port http://localhost:${PORT}🚀🚀`);
 
 app.listen(PORT, handleListening);
