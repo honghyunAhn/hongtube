35bd48d537b1a6cfe6f7849fde52dae652c1ab47
honghyunAhn <anh2073@naver.com>
2021-08-07 17:09:13 +0900

    Middlewares part One

--- src/server.js
+++ src/server.js
@@ -4,6 +4,20 @@ const PORT = 9090;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+}
+const handleHome = (req, res) => {
+    return res.send("I love middlewares");
+}
+const handleLogin = (req, res) => {
+    return res.send({message:"Login here."});
+}
+
+app.get("/", gossipMiddleware, handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
