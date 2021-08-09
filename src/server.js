56bf431488f557053e16fd12a71978713d4dd156
honghyunAhn <anh2073@naver.com>
2021-08-09 23:00:11 +0900

    Add : morgan

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 import express from "express";
+import logger from "morgan";
 
 const PORT = 9090;
+
 const app = express();
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
+const home = (req, res) => {
+  return res.send("hello");
+};
+const login = (req, res) => {
+  return res.send("login");
 };
 
-const handleHome = (req, res) => {
-    return res.send("I love middlewares.");
-};
-
-app.get("/", logger, handleHome);
+app.use(logger("dev"));
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () => 
     console.log(`✅Server listenting on port http://localhost:${PORT}🚀🚀`);
