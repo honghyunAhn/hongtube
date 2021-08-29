8b64c6f013bb387c322925e94f496fc56362b4c7
honghyunAhn <anh2073@naver.com>
2021-08-30 01:15:09 +0900

    feat : Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,7 @@
-const fakeUser = {
-    username: "anh288",
-    loggedIn: false,
+export const trending = (req, res) => {
+    const videos = [1,2,3,4,5,6,7,8,9,10];
+    return res.render("home", {pageTitle: "Home", videos});
 }
-
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser: fakeUser});
 export const see = (req, res) => res.render("watch", {pageTitle: "Watch Video"});
 export const edit = (req, res) => res.render("edit", {pageTitle: "Edit Video"});
 export const search = (req, res) => res.send("Search");
