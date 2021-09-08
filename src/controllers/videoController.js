489150462bc3614a5bfa431919a280b9cbc2e31b
honghyunAhn <anh2073@naver.com>
2021-09-09 01:10:33 +0900

    feat : Search part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -65,10 +65,15 @@ export const deleteVideo = async(req, res) => {
   return res.redirect("/");
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const { keyword } = req.query;
-  if(keyword){
-    
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}`, "i"),
+      }
+    });
   }
-  return res.render("search", {pageTitle:"Search"})
-}
+  return res.render("search", { pageTitle: "Search", videos });
+};
